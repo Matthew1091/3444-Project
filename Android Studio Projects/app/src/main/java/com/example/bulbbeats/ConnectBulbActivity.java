6f0276da8d65ed5a53886c211f8931428ee0a7ca
Malena <reyesamalena@gmail.com>
2019-10-18 18:50:05 -0500

    Added ConnectBulbActivity, LaunchActivity, SongActivity, ThemeActivity. ConnectBulbActivity: changed to RelativeLayout, added bluetooth image; textview; and connectButton

---
+++ Android Studio Projects/app/src/main/java/com/example/bulbbeats/ConnectBulbActivity.java
@@ -0,0 +1,14 @@
+package com.example.bulbbeats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConnectBulbActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect_bulb);
+    }
+}
