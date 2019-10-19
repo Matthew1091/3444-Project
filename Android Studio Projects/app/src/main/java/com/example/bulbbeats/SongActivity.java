c0c50d150e81b269d6ee0458b133b55afe7f39b4
Malena <reyesamalena@gmail.com>
2019-10-18 23:28:08 -0500

    LaunchActivity xml:changed to xml RelativeLayout, added pause, play stop button; textview; and bulb logo SongActivity xml:changed to xml RelativeLayout, added spinner, added upload button; textview; and music image ThemeActivity xml:changed to xml RelativeLayout, added spinner,select button; textview; and settings image

--- Android Studio Projects/app/src/main/java/com/example/bulbbeats/SongActivity.java
+++ Android Studio Projects/app/src/main/java/com/example/bulbbeats/SongActivity.java
@@ -2,13 +2,20 @@ package com.example.bulbbeats;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class SongActivity extends AppCompatActivity {
 
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_song);
+
     }
 }
