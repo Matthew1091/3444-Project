37d0e19869c56f96495056a621ae61797f29fb31
Malena <reyesamalena@gmail.com>
2019-10-22 19:34:32 -0500

    added intents for process flow and reformatted main activity a tad

--- Android Studio Projects/app/src/main/java/com/example/bulbbeats/ConnectBulbActivity.java
+++ Android Studio Projects/app/src/main/java/com/example/bulbbeats/ConnectBulbActivity.java
@@ -2,13 +2,38 @@ package com.example.bulbbeats;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConnectBulbActivity extends AppCompatActivity {
 
+    private Button connectBulbbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect_bulb);
+
+        initConnectBulbWidgits();
+        setConnectBulbOnClickListeners();
     }
+
+    private void initConnectBulbWidgits(){
+        connectBulbbtn = (Button) findViewById((R.id.connectBtn));
+    }
+
+    private void setConnectBulbOnClickListeners(){
+        connectBulbbtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) { //will start from connectbulb to songactivity class
+               Intent cintent = new Intent(ConnectBulbActivity.this, SongActivity.class);
+               startActivity(cintent); //will trigger the intent
+           }
+       });
+
+}
+
+
+
 }
