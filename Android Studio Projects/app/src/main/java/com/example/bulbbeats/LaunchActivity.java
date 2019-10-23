fb1aa324895e34e64c4dcf913319d3a839c8779f
Malena <reyesamalena@gmail.com>
2019-10-22 20:52:21 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 6d1ebbf2f3294be9c25590258e60c0987bca1fe9, reversing
    changes made to 37d0e19869c56f96495056a621ae61797f29fb31.

--- Android Studio Projects/app/src/main/java/com/example/bulbbeats/LaunchActivity.java
+++ Android Studio Projects/app/src/main/java/com/example/bulbbeats/LaunchActivity.java
@@ -2,31 +2,13 @@ package com.example.bulbbeats;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 public class LaunchActivity extends AppCompatActivity {
-
-    ImageButton Play;
-    TextView songTitle;
-    ProjectSettings projSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
-
-        Intent intent = getIntent();
-
-        projSet = intent.getParcelableExtra("openDoc");
-
-        Play = findViewById((R.id.playBtn));
-
-        /*if(projSet != null) {
-            songTitle.setText(projSet.songUri.getLastPathSegment());
-        }*/// this code makes it crash for some reason, but projSet does successfully come through.
     }
 }
