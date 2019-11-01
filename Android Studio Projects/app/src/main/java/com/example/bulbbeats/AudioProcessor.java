60046568f281768a0f19c8ee254163108ad2de84
AMosesM <55302718+AMosesM@users.noreply.github.com>
2019-11-01 15:52:50 -0500

    FFT capture
    Debug output shows the frequency bins. Runs at 50ms on the emulator. Interested in seeing how it preforms on differency environments.

--- Android Studio Projects/app/src/main/java/com/example/bulbbeats/AudioProcessor.java
+++ Android Studio Projects/app/src/main/java/com/example/bulbbeats/AudioProcessor.java
@@ -8,8 +8,9 @@ import android.util.Log;
 import java.util.Date;
 
 public class AudioProcessor{
-    private byte bytes[];
+    private float FFT[];
     private Visualizer mVisualizer;
+    private static int numBins = 16;
     private int PERMISSION_CODE = 1;
 
     Date date1 = new Date();
@@ -30,22 +31,31 @@ public class AudioProcessor{
 
             @Override
             public void onFftDataCapture(Visualizer visualizer, byte[] fft, int samplingRate) {
+                FFT = new float[fft.length];
                 updateFFT(fft);
             }
         };
 
         //this sets up the listener but does not actually enable the visualizer. That happens back in launch.
         mVisualizer.setDataCaptureListener(captureListener,
-                Visualizer.getMaxCaptureRate() / 2, false, true);
+                20000, false, true);
     }
 
     public void updateFFT(byte[] fft)
     {
         date2 = new Date();
-        bytes = fft;
+
+        for (int i = 0; i < FFT.length / numBins; i++)
+        {
+            FFT[i] = 0;
+            byte rfk = fft[numBins * i];
+            byte ifk = fft[numBins * i + 1];
+            FFT[i] = (int) Math.log10(rfk * rfk + ifk *ifk) * 4;
+        }
+
         long capture = date2.getTime() - date1.getTime();
-        Log.d("Updating FFT", String.valueOf(capture) + " milliseconds");
-        date1 = null;
+        Log.d("Updating FFT", String.format("%d ms -- %6.1f %6.1f %6.1f %6.1f %6.1f %6.1f %6.1f %6.1f %6.1f %6.1f %6.1f %6.1f %6.1f %6.1f %6.1f %6.1f",
+                capture, FFT[0], FFT[1], FFT[2], FFT[3],FFT[4], FFT[5], FFT[6], FFT[7],FFT[8], FFT[9], FFT[10], FFT[11],FFT[12], FFT[13], FFT[14], FFT[15]));
         date1 = new Date();
     }
 
@@ -54,9 +64,9 @@ public class AudioProcessor{
         mVisualizer.release();
     }
 
-    public byte[] getBytesFFT()
+    public float[] getBytesFFT()
     {
-        return bytes;
+        return FFT;
     }
 
     public void enable()
